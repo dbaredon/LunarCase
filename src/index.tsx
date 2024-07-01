d316f112f417f7c251c38aea6cba2b4bc5ea8e1c
Søren Ditlev <sdd@lunar.app>
2024-07-01 13:45:15 +0200

    ⬆️ Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/client';
 import 'normalize.css';
 import { App } from './App';
 import { createClient } from './graphql_server/mock_server';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 
 const run = async () => {
   const client = await createClient();
+  const container = document.getElementById('root');
+  const root = createRoot(container!);
 
-  ReactDOM.render(
+  root.render(
     <React.StrictMode>
       <ApolloProvider client={client}>
         <App />
       </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
   );
 };
 
